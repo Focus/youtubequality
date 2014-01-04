c84167a3bc9d7dfb452b0c0c5a3c05826c7e4c48
Focus <batieatworld@gmail.com>
2014-01-04 00:27:32 +0000

    Tidy up and fix minor problems

--- scripts/yt.js
+++ scripts/yt.js
@@ -4,15 +4,18 @@ var pause = false;
 function injectCode(){
 		var inj0 = document.createElement('script');
 		var inj1 = document.createElement('script');
+		var docFrag = document.createDocumentFragment();
 		inj0.type = "text/javascript";
-		inj0.src = chrome.extension.getURL('utils.js');
+		inj0.src = chrome.extension.getURL('scripts/utils.js');
 		
 		inj1.innerHTML =
 				["function onYouTubePlayerReady(player){",
 				 "		setTimeout(function(){ytPlayerHook(player," + quality + "," + pause + ");},10);",
 				 "}"].join('\n');
-		document.head.appendChild(inj0);
-		document.head.appendChild(inj1);
+		docFrag.appendChild(inj0);
+		docFrag.appendChild(inj1);
+		
+		document.head.appendChild(docFrag);
 }
 
 chrome.extension.sendRequest({method: "getStatus"}, function(response) {
