468bcfe9b1e3dc64f5dafb988554a651c67c7b8b
Philipp Kitzberger <philipp@kitze.net>
2014-07-22 18:14:40 +0200

    New feature: keep set quality as max quality

--- pages/options.js
+++ pages/options.js
@@ -4,6 +4,11 @@ function save_options() {
 	chrome.extension.getBackgroundPage().pause = true;
   else
 	chrome.extension.getBackgroundPage().pause = false;
+  localStorage['ytKeepQuality'] = document.getElementById("keepquality").checked;
+  if(localStorage['ytKeepQuality'] === "true")
+	chrome.extension.getBackgroundPage().keepquality = true;
+  else
+	chrome.extension.getBackgroundPage().keepquality = false;
 }
 
 function restore_options() {
@@ -12,6 +17,12 @@ function restore_options() {
 	document.getElementById("pause").checked = true;
   else
 	document.getElementById("pause").checked = false;
+  var keepquality = localStorage["ytKeepQuality"];
+  if(keepquality === "true")
+	document.getElementById("keepquality").checked = true;
+  else
+	document.getElementById("keepquality").checked = false;
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#pause').addEventListener('change', save_options);
+document.querySelector('#keepquality').addEventListener('change', save_options);
--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,14 @@
 var res = localStorage['ytQuality'];
 var pause = false;
+var keepquality = false;
+
 if(localStorage['ytPause'] === "true")
 		pause = true;
+if(localStorage['ytKeepQuality'])
+		keepquality = true;
 if(isNaN(res) || res > 8 || res < 0)
 		res = 4;
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 		if (request.method === "getStatus"){
 				if(res < 8 && res >=0)
@@ -13,6 +18,8 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 		}
 		else if (request.method === "getPause")
 				sendResponse({status: pause});
+		else if (request.method === "getKeepQuality")
+				sendResponse({status: keepquality});
 		else
 				sendResponse({});
 });
